71b52bfa3ed410e286939abc99d468ed2e8d6928
Cristian Rios <cristianriosv@gmail.com>
2023-10-04 16:31:36 +0200

    feat: create weather data props type

---
+++ src/types/weatherDataProps.d.ts
@@ -0,0 +1,10 @@
+type WeatherDataProps = {
+    cityName: string;
+    temperature: number;
+    feelsLike: number;
+    humidity: number;
+    windSpeed: number;
+    weatherDescription: string;
+    weatherIcon: string;
+    pressure: number;
+}
